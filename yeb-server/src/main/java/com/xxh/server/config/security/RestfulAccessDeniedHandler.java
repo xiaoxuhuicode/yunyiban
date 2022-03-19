2e43681c41981f783a3738c2692e0e6ca18b481b
xiaoxuhuicode <2391019497@qq.com>
2022-03-19 17:49:18 +0800

    云e办后端Security登录授权过滤器以及自定义返回结果

---
+++ yeb-server/src/main/java/com/xxh/server/config/security/RestfulAccessDeniedHandler.java
@@ -0,0 +1,34 @@
+package com.xxh.server.config.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.xxh.server.pojo.RespBean;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author xxh
+ * @since 2022/3/19 17:40
+ * 当访问接口没有权限，自定义返回结果
+ */
+@Component
+public class RestfulAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+        RespBean bean = RespBean.error("权限不足，请联系管理员！");
+        bean.setCode(403);
+        out.write(new ObjectMapper().writeValueAsString(bean));
+        out.flush();
+        out.close();
+    }
+}
---
+++ yeb-server/src/main/java/com/xxh/server/config/security/SecurityConfig.java
@@ -0,0 +1,84 @@
+package com.xxh.server.config.security;
+
+import com.xxh.server.pojo.Admin;
+import com.xxh.server.service.IAdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+/**
+ * @author xxh
+ * @since 2022/3/19 16:26
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private IAdminService adminService;
+    @Autowired
+    private RestAuthorizationEntryPoint restAuthorizationEntryPoint;
+    @Autowired
+    private RestfulAccessDeniedHandler restfulAccessDeniedHandler;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //使用jwt，不需要csrf
+        http.csrf()
+                .disable()
+                //基于token,不需要session
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                //允许登录访问
+                .antMatchers("/login", "logout")
+                .permitAll()
+                //除了上面的，所有请求都要求认证
+                .anyRequest()
+                .authenticated()
+                .and()
+                .headers()
+                .cacheControl();
+        //添加jwt,登录授权过滤器
+        http.addFilterBefore(jwtAuthencationTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+        //添加自定义未授权和未登录结果返回
+        http.exceptionHandling()
+                .accessDeniedHandler(restfulAccessDeniedHandler)
+                .authenticationEntryPoint(restAuthorizationEntryPoint);
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        return username -> {
+            Admin admin = adminService.getAdminByUserName(username);
+            if (null != admin) {
+                return admin;
+            }
+            return null;
+        };
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JwtAuthencationTokenFilter jwtAuthencationTokenFilter() {
+        return new JwtAuthencationTokenFilter();
+    }
+}
